9cd88b037f3e46defd66cfc5051fda063d811db5
Daneybis Calero <daneybis2008@gmail.com>
2024-05-25 16:58:24 -0600

    Guia POO independiente

--- src/Camion.java
+++ src/Camion.java
@@ -19,4 +19,16 @@ public class Camion extends Vehiculocarga {
         super.mostrarInfo();
         System.out.println("Número de ejes: "+numEjes);
     }
+
+    public void realizarMantenimientoCa(){
+        System.out.println("El camión debe realizar este mantenimiento:");
+        System.out.println("1) Análisis de los mecanismos de dirección.");
+        System.out.println("2) Revisión de los niveles de lubricantes.");
+        System.out.println("3) Inspección de los sistemas de combustible y escape.");
+    }
+
+    public int calcularCostoMantenimiento1(int costoporkm1, int kilometrosRecorridos1){
+        int costoTotal = costoporkm1 * kilometrosRecorridos1;
+        return costoTotal;
+    }
 }
